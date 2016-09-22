277f5e5630dcfdfd620b6334eba3af0a5768d1ec
rider7 <brandon.rider@gmail.com>
2016-09-22 14:32:16 -0500

    study page works, can switch to quiz page, need to create quiz

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//file defines our app
+(function() {
+    //immediately invoking function expression, runs automatically
+    angular.module("turtleFacts", []);
+    //create angular module, name same as ng-app on index, array will contain dependecies of module
+})()
---
+++ js/controllers/quiz.js
@@ -0,0 +1,12 @@
+(function() {
+    angular.module("turtleFacts")
+        .controller("quizCtrl", QuizController)
+
+        QuizController.$inject = ["quizMetrics"];
+
+    function QuizController(quizMetrics) {
+        var vm = this;
+        vm.quizMetrics = quizMetrics;
+
+    }
+})()
