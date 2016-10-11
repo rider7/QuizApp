9eb1ae49366040f0d7f0a895a3f3e746fbc90ece
rider7 <brandon.rider@gmail.com>
2016-10-11 12:25:27 -0500

    set up uirouter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 //file defines our app
 (function() {
     //immediately invoking function expression, runs automatically
-    angular.module("csharpFacts", []);
+    angular.module("csharpFacts", ['ui.router']) // injects ui-router
     //create angular module, name same as ng-app on index, array will contain dependecies of module
+
+    .config(function($stateProvider, $urlRouterProvider){
+
+      $urlRouterProvider.otherwise('/home');
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'routes/quizMain.html'
+        })
+
+        .state('admin', {
+          url: '/admin',
+          templateUrl: 'routes/quizAdmin.html'
+        })
+
+    });
+
 })()
